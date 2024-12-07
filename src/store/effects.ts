b82d6fd9d22fe659d626dc9bd47c10ed0c4c616f
Zaven477 <Papikyan86@bk.ru>
2024-12-07 20:59:13 +0400

    processing the request result

---
+++ src/store/effects.ts
@@ -0,0 +1,29 @@
+import { getExchangeRates } from "../api/dataExchangeRates";
+import { AppDispatch } from "./configureStore";
+import {
+  setCurrentDate,
+  setError,
+  setExchangeCurrency,
+  setLoading,
+} from "./exchangeRatesSlice";
+
+export const fetchDataExchangeRates = () => {
+  return async (dispatch: AppDispatch) => {
+    try {
+      dispatch(setLoading(true));
+
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      const { Valute, Date } = await getExchangeRates();
+      dispatch(setExchangeCurrency(Valute));
+      dispatch(setCurrentDate(Date));
+
+      dispatch(setLoading(false));
+    } catch (error) {
+      if (error instanceof Error) {
+        dispatch(setLoading(false));
+        dispatch(setError(error.message));
+      }
+    }
+  };
+};
